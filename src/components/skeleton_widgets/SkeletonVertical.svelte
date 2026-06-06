7cfb8aa021547a3a0ff61d47043d1019396b2d04
Pavel Kholkin <xpaul6@gmail.com>
2026-06-06 22:06:11 +0500

    feat: skeleton layout for editor

---
+++ src/components/skeleton_widgets/SkeletonVertical.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import SkeletonBuilder from "./SkeletonBuilder.svelte";
+
+  const { settings, widgets } = $props();
+  const { k, hs } = $derived(settings);
+</script>
+
+<div class="h-full flex flex-col shrink-0 border border-gray-500 rounded-sm p-2">
+  {#each hs as h, i}
+    <div class={`w-full p-2`} style={`height: ${h}%;`}>
+      <SkeletonBuilder widget={{ ...widgets[i] }} />
+    </div>
+  {/each}
+</div>
--- src/components/widgets/MasterWidget.svelte
+++ src/components/widgets/MasterWidget.svelte
@@ -1,85 +1,8 @@
 <script lang="ts">
-    import type { NewsItem } from "@/lib/dto";
     import BuilderWidget from "./BuilderWidget.svelte";
     import EmergencyWidget from "./EmergencyWidget.svelte";
     import HeaderWidget from "./HeaderWidget.svelte";
-
-    const data = {
-        type: "vertical",
-        settings: {
-            k: 3,
-            hs: [40, 40, 20],
-        },
-        data: [
-            {
-                type: "news",
-                settings: {},
-                data: {
-                    news: [
-                        {
-                            title: "Title #1",
-                            text: "adjaragudzhu",
-                            date: "2026-06-06",
-                        } as NewsItem,
-                        {
-                            title: "Title #2",
-                            text: '<p style="color: red;">lolxd</p>',
-                            date: "2026-06-06",
-                        } as NewsItem,
-                        {
-                            title: "Title #3",
-                            text: "adjaragudzhu",
-                            date: "2026-06-06",
-                        } as NewsItem,
-                    ],
-                },
-            },
-            {
-                type: "horizontal",
-                settings: {
-                    k: 2,
-                    ws: [60, 40],
-                },
-                data: [
-                    {
-                        type: "static",
-                        data: {
-                            title: "1",
-                            text: "1",
-                        },
-                    },
-                    {
-                        type: "vertical",
-                        settings: {
-                            k: 2,
-                            hs: [50, 50],
-                        },
-                        data: [
-                            {
-                                type: "static",
-                                data: {
-                                    title: "2",
-                                    text: "2",
-                                },
-                            },
-                            {
-                                type: "static",
-                                data: {
-                                    title: "3",
-                                    text: "3",
-                                },
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                type: "dynamic",
-                settings: {},
-                data: {},
-            },
-        ],
-    };
+    import data from "../../lib/mock_data";
 
     const emergency = false;
 </script>
@@ -91,10 +14,7 @@
         <HeaderWidget />
     {/if}
 
-    <!-- {#each data as widget}
-        <BuilderWidget {widget} />
-    {/each} -->
-    <div class="h-[95%]">
+    <div class="h-[95%] p-1">
         <BuilderWidget widget={data} />
     </div>
 </div>
