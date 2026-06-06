090837af4c49e972aa13eda6e70837a3681bcec4
p0ancf-36 <bakhmatov.eugene@yandex.ru>
2026-06-06 19:46:03 +0500

    fit: widgets system

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -1,78 +1,80 @@
 <script lang="ts">
-  import { useParams } from "@hvniel/svelte-router";
-  import type { NewsItem } from "../lib/dto";
+    import { useParams } from "@hvniel/svelte-router";
+    import type { NewsItem } from "../lib/dto";
 
-  import NewsWidget from "./widgets/NewsWidget.svelte";
-  import StaticDataWidget from "./widgets/StaticDataWidget.svelte"
+    import NewsWidget from "./widgets/NewsWidget.svelte";
+    import StaticDataWidget from "./widgets/StaticDataWidget.svelte";
+    import MasterWidget from "./widgets/MasterWidget.svelte";
 
-  const { id } = $derived(useParams());
-  let datetime = $state(new Date());
+    const { id } = $derived(useParams());
+    let datetime = $state(new Date());
 
-  let isEmergency = $state<Boolean>(false);
-  let news = $state<NewsItem[]>([
-    {
-      title: "Title #1",
-      text: "adjaragudzhu",
-      date: "2026-06-06"
-    } as NewsItem,
-    {
-      title: "Title #2",
-      text: "<p style=\"color: red;\">lolxd</p>",
-      date: "2026-06-06"
-    } as NewsItem,
-    {
-      title: "Title #3",
-      text: "adjaragudzhu",
-      date: "2026-06-06"
-    } as NewsItem,
-  ]);
+    let isEmergency = $state<Boolean>(false);
+    let news = $state<NewsItem[]>([
+        {
+            title: "Title #1",
+            text: "adjaragudzhu",
+            date: "2026-06-06",
+        } as NewsItem,
+        {
+            title: "Title #2",
+            text: '<p style="color: red;">lolxd</p>',
+            date: "2026-06-06",
+        } as NewsItem,
+        {
+            title: "Title #3",
+            text: "adjaragudzhu",
+            date: "2026-06-06",
+        } as NewsItem,
+    ]);
 
-  function formatDateTime(v: Date): string {
-    let str = v.toLocaleDateString("ru-RU", {
-      weekday: "long",
-      day: "numeric",
-      month: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
+    function formatDateTime(v: Date): string {
+        let str = v.toLocaleDateString("ru-RU", {
+            weekday: "long",
+            day: "numeric",
+            month: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    $effect(() => {
+        setInterval(() => {
+            datetime = new Date();
+        }, 1000 * 30);
     });
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-  $effect(() => {
-    setInterval(() => {
-      datetime = new Date();
-    }, 1000 * 30);
-  });
 </script>
 
 <svelte:head>
-  <title>Dashboard</title>
+    <title>Dashboard</title>
 </svelte:head>
 
 <main
-  class={`flex flex-col justify-between h-screen w-full border border-red-500 ${isEmergency ? "bg-red-500" : ""}`}
+    class={`flex flex-col justify-between h-screen w-full border border-red-500 ${isEmergency ? "bg-red-500" : ""}`}
 >
-  {#if isEmergency}
-    <div
-      class="h-[5%] p-1 m-1 border border-green-500 bg-red-500 text-white text-2xl text-center"
-    >
-      AAAAAAAAAAAAAAAAAAAAAAAAAA
+    {#if isEmergency}
+        <div
+            class="h-[5%] p-1 m-1 border border-green-500 bg-red-500 text-white text-2xl text-center"
+        >
+            AAAAAAAAAAAAAAAAAAAAAAAAAA
+        </div>
+    {:else}
+        <div
+            class="h-[5%] p-1 m-1 border border-green-500 flex flex-row justify-between"
+        >
+            <div>({id})</div>
+            <div>{formatDateTime(datetime)}</div>
+        </div>
+    {/if}
+    <div class="h-full p-1 m-1 border border-blue-500 bg-white">
+        <NewsWidget {news} />
     </div>
-  {:else}
-    <div
-      class="h-[5%] p-1 m-1 border border-green-500 flex flex-row justify-between"
-    >
-      <div>({id})</div>
-      <div>{formatDateTime(datetime)}</div>
+    <div class="h-[40%] p-1 m-1 border border-purple-500 bg-white">
+        <StaticDataWidget />
     </div>
-  {/if}
-  <div class="h-full p-1 m-1 border border-blue-500 bg-white">
-    <NewsWidget news={news} />
-  </div>
-  <div class="h-[40%] p-1 m-1 border border-purple-500 bg-white">
-    <StaticDataWidget />
-  </div>
-  <div class="h-[15%] p-1 m-1 border border-yellow-500 bg-white">
-    Dynamic block
-  </div>
+    <div class="h-[15%] p-1 m-1 border border-yellow-500 bg-white">
+        Dynamic block
+    </div>
+    <MasterWidget />
 </main>
---
+++ src/components/widgets/MasterWidget.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+    import type { NewsItem } from "@/lib/dto";
+    import BuilderWidget from "./BuilderWidget.svelte";
+    import EmergencyWidget from "./EmergencyWidget.svelte";
+    import HeaderWidget from "./HeaderWidget.svelte";
+
+    const data = [
+        {
+            type: "news",
+            height: 40,
+            settings: {},
+            data: {
+                news: [
+                    {
+                        title: "Title #1",
+                        text: "adjaragudzhu",
+                        date: "2026-06-06",
+                    } as NewsItem,
+                    {
+                        title: "Title #2",
+                        text: '<p style="color: red;">lolxd</p>',
+                        date: "2026-06-06",
+                    } as NewsItem,
+                    {
+                        title: "Title #3",
+                        text: "adjaragudzhu",
+                        date: "2026-06-06",
+                    } as NewsItem,
+                ],
+            },
+        },
+        {
+            type: "horizontal",
+            height: 40,
+            settings: {
+                k: 2,
+                ws: [60, 40],
+            },
+            data: [
+                {
+                    type: "static",
+                    data: {
+                        title: "1",
+                        text: "1",
+                    },
+                },
+                {
+                    type: "vertical",
+                    settings: {
+                        k: 2,
+                        hs: [50, 50],
+                    },
+                    data: [
+                        {
+                            type: "static",
+                            data: {
+                                title: "2",
+                                text: "2",
+                            },
+                        },
+                        {
+                            type: "static",
+                            data: {
+                                title: "3",
+                                text: "3",
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            type: "dynamic",
+            height: 20,
+            settings: {},
+            data: {},
+        },
+    ];
+
+    const emergency = false;
+</script>
+
+<div class="h-screen">
+    {#if emergency}
+        <EmergencyWidget />
+    {:else}
+        <HeaderWidget />
+    {/if}
+
+    {#each data as widget}
+        <BuilderWidget {widget} />
+    {/each}
+</div>
